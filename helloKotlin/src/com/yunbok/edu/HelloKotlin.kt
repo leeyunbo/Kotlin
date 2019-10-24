c2e195afcabed2b2a4a6303c5d530bafc0423464
leeyunbo <servers1@naver.com>
2019-10-24 22:31:42 +0900

    Do it!, 연습 1주차

---
+++ helloKotlin/src/com/yunbok/edu/HelloKotlin.kt
@@ -0,0 +1,5 @@
+package com.yunbok.edu
+
+fun main() {
+    print("Hello Kotlin") //디 컴파일러 하면, 자동으로 클래스 생성되어 내부 함수로 설정. 따라서 따로 클래스 선언 안해도 컴파일 가능
+}
---
+++ helloKotlin/src/com/yunbok/edu/Person.kt
@@ -0,0 +1,3 @@
+package com.yunbok.edu
+
+class Person (val name : String, val age : Int)
