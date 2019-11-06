ecfb1053d77be19fb35bfe93e8e91bf3df0eb57f
leeyunbo <servers1@naver.com>
2019-11-06 15:19:23 +0900

    Do it!, 연습 3주차

--- helloKotlin/src/chap03/section1/ParamCount.kt
+++ helloKotlin/src/chap03/section1/ParamCount.kt
@@ -12,12 +12,27 @@ fun main() {
     moreParam{a,b -> "hello World! $a, $b"}                  //어디서든 소괄호는 생략이 가능하다.
     moreParam{_,b -> "Hello World $b"}                        //둘 중 하나의 인자를 사용하지 않으려면 "_" 기호로 생략이 가능하다.
     """------------------------------------------------------------------------------------------------------------------"""
-    
+    witArgs("common","common"){a,b -> "Hello World $a $b"} // 이런식으로 간략화 하기 위해서는 람다식 함수가 마지막 배개변수로 위치해야함
+    witArgs("common", "common", {a,b -> "Hello World $a $b"})
+    """------------------------------------------------------------------------------------------------------------------"""
+    twoLambda({a,b -> "Hello World $a, $b"},{"Hello World $it"}) //람다식 두개 이상이 매개 변수로 들어간다면 소괄호는 생략이 불가능
+    twoLambda({a,b -> "Hello World $a $b"}) {"Hello World $it"} // 생략은 불가능하지만, 마지막 함수를 소괄호 밖에 둘 수 있음
 
 
 
 
 
+
+
+
+
+}
+fun twoLambda(first : (String, String) -> String, second : (String) -> String) {
+    println(first("hello", "hello"))
+    println(second("hello"))
+}
+fun witArgs(a : String, b : String ,out : (String, String) -> String) {
+    println(out(a,b))
 }
 fun moreParam(out : (String, String) -> String) {
     println(out("moreParam","moreParam"))
