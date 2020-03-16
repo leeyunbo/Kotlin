22b5fb92830833bba8514b7b1fd3f4f8c28f01f8
leeyunbo <servers1@naver.com>
2020-03-16 15:02:43 +0900

    코틀린 챕터8 Array 클래스에 대한 이해

--- helloKotlin/src/chap08/ArraySortedSortWith.kt
+++ helloKotlin/src/chap08/ArraySortedSortWith.kt
@@ -1,5 +1,10 @@
 package chap08
 
+/**
+ * comparator 객체를 이용하는 방법. 만약 오른쪽으로 갈꺼면 1, 왼쪽으로 갈꺼면 -1, 그대로 둘꺼면 0
+ * compareBy를 이용하는 방법 => it.name, it.price를 인자로 넘겨줌으로써 두 가지 키에 의한 정렬이 구현됨. name이 우선
+ */
+
 private data class Products(val name : String, val price : Double)
 
 fun main() {
