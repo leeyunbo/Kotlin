7562ee4dd38c0443ca8333493369f2c40cad8144
leeyunbo <servers1@naver.com>
2020-03-22 20:29:08 +0900

    코틀린 챕터10 DSL

--- helloKotlin/src/chap10/DSLEx.kt
+++ helloKotlin/src/chap10/DSLEx.kt
@@ -1,5 +1,14 @@
 package chap10
 
+/**
+ * DSL 예제,
+ * 특정 어플리케이션의 도메인을 위해 특화된 언어. 람다 함수와 확장 함수를 적절히 활용하면 DSL 형태의 언어를 만들 수 있음
+ * 대표적으로 Anko 프레임워크가 있다.
+ *
+ * (DslPerson) -> Unit => DslPerson 객체를 인자로 받는 람다 함수기 때문에, it으로 접근
+ * DslPerson.() -> Unit => 람다 함수를 호출한 것이 DslPerson 객체이기 때문에, this로 접근이 가능해진다.
+ * 거기에 apply()를 사용했기 때문에, this를 생략이 가능하다.
+ */
 data class DslPerson (
     var name : String? = null,
     var age : Int? = null,
@@ -24,6 +33,15 @@ fun DslPerson.job(block : DslJob.() -> Unit) {
 }
 
 fun main() {
+    val personFirst = personFirst {
+        it.name = "Kildong"
+        it.age = 24
+        it.job {
+            category = "IT"
+            position = "Android Developer"
+            extension = 1234
+        }
+    }
     val person = person {
         name = "Kildong"
         age = 40
