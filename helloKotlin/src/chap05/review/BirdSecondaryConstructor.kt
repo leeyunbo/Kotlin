23c4c8310e22d63b5a067633b973ab1757613b20
leeyunbo <servers1@naver.com>
2020-04-07 15:31:40 +0900

    코틀린 챕터 5 클래스 및 생성자

---
+++ helloKotlin/src/chap05/review/BirdSecondaryConstructor.kt
@@ -0,0 +1,26 @@
+package chap05.review
+
+private class Bird {
+    var name : String
+    var wing : Int
+    var beak : String
+    var color: String
+
+    constructor(name: String, wing: Int, beak: String, color: String) {
+        this.name = name
+        this.wing = wing
+        this.beak = beak
+        this.color = color
+    }
+
+    fun fly() = println("Fly wing : $wing")
+    fun sing(vol: Int) = println("Sing vol: $vol")
+}
+
+fun main() {
+    val coco = Bird("mybird", 2, "short", "blue")
+
+    coco.color = "yellow"
+    coco.fly()
+    coco.sing(3)
+}
