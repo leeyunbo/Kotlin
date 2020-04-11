f1468638ada3e4cf5beefb1c8a6aa8d6281b325e
leeyunbo <servers1@naver.com>
2020-04-11 16:27:22 +0900

    코틀린 챕터 5 프로퍼티 get/set

---
+++ helloKotlin/src/chap06/review/NormalGetterSetter.kt
@@ -0,0 +1,36 @@
+package chap06.review
+
+/**
+ * field : 클래스의 프로퍼티를 참조하는 변수
+ * value : 세터의 매개변수로 외부로터 값을 가져옴
+ * 만약 프로퍼티에 직접 접근하면 각각, setValue, getValue로 자동변환되어 무한 호출됨
+ * 따라서 StackOverFlow가 발생
+ * set에 private 사용 가능, 그럼 변수 데이터를 바꿔줄 수 없음
+ * 근데.. 그럴꺼면 그냥 val로 하면 되자나?
+ */
+
+class User(_id: Int, _name: String, _age: Int) {
+    val id: Int = _id
+        get() = field
+
+    var name: String = _name
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var age: Int = _age
+        get() = field
+        set(value) {
+            field = value
+        }
+
+
+}
+
+
+fun main() {
+    val user1 = User(1, "kildong", 30)
+    user1.age = 35
+    println("user1.age = ${user1.age}")
+}
